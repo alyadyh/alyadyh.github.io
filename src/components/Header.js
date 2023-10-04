792e195e68caf92a17091e072c83bff808c5e385
alyadyh <alya.mardhiyyah@binus.ac.id>
2023-10-05 00:25:56 +0700

    customize header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import Logo from '../assets/logo.svg';
+
 const Header = () => {
-  return <div className='bg-pink-300'>Header</div>;
+  return (
+    <header className='py-8'>
+      <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          <a href='#' className='h-[75px] w-[75px]'>
+            <img src={Logo} alt=''/>
+          </a>
+
+          <button className='btn btn-sm'>Work with me</button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
