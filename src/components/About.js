6ab5906d70a555ca49ea743061d0fa1caabf150b
alyadyh <alya.mardhiyyah@binus.ac.id>
2023-10-10 12:48:41 +0700

    arrange icon

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ const About = () => {
   });
   return (
     <section className='section' id='about' ref={ref}>
-      <div className="container mx-auto">
+      <div className="container mx-auto mt-[45px]">
         <div className='flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
           <motion.div
             variants={fadeIn('right', 0.3)}
@@ -26,15 +26,16 @@ const About = () => {
           </motion.div>
           <div className='flex-1'>
             <h2 className='h2 font-semibold text-accent'>About Me.</h2>
-            <h5 className='h5 text-[14px] lg:text-[18px] mb-4 font-secondary font-light'>I'm a 5th semester Computer Science student who passionate in web development.</h5>
-            <p className='mb-8'></p>
+            <h5 className='h5 text-[13px] lg:text-[18px] mb-4 font-secondary font-light'>Hello! Interestingly, I got introduced to programming in 
+            freshman year at <span text-accent>Bina Nusantara University, Indonesia</span> where I study my Bachelors of Computer Science (2021-2025).</h5>
+            <h5 className='h5 text-[13px] lg:text-[18px] mb-4 font-secondary font-light'>Here is a list of the technologies that familiar with!</h5>
 
             <div className='flex gap-x-6 lg:gap-x-10 mb-12'>
               <div>
                 <div className='text-[40px] font-tertiary text-gradient mb-2'>
                   {inView ? <CountUp start={0} end={2} duration={3} /> : null}
                 </div>
-                <div className='font-primary text-sm tracking-[2px]'>
+                <div className='font-primary text-xs lg:text-sm tracking-[2px]'>
                   Years of <br/>
                   Organization
                 </div>
@@ -43,7 +44,7 @@ const About = () => {
                 <div className='text-[40px] font-tertiary text-gradient mb-2'>
                   {inView ? <CountUp start={0} end={13} duration={3} /> : null}
                 </div>
-                <div className='font-primary text-sm tracking-[2px]'>
+                <div className='font-primary text-xs lg:text-sm tracking-[2px]'>
                   Projects <br/>
                   Completed
                 </div>
@@ -52,37 +53,46 @@ const About = () => {
                 <div className='text-[40px] font-tertiary text-gradient mb-2'>
                   {inView ? <CountUp start={0} end={4} duration={3} /> : null}
                 </div>
-                <div className='font-primary text-sm tracking-[2px]'>
+                <div className='font-primary text-xs lg:text-sm tracking-[2px]'>
                   Certification <br/>
                   Earned
                 </div>
               </div>
             </div>
             <div className='flex gap-x-8 items-center'>
-              <button className='btn btn-lg'>Download my Resume</button>
+              <button className='btn btn-lg'>Download my CV</button>
             </div>
           </div>
         </div>
-        <div className='flex flex-col lg:flex-row'>
+
+        <div className='flex flex-col lg:flex-row mt-18 mb-32'>
           <div className='flex-1 mb-12 lg:mb-0'>
-            <h2 className='h2 text-accent mb-6'>My Certifications</h2>
-            <div className='flex text-[25px] gap-x-6 max-w-max mx-auto lg:mx-0 mb-16'>
-              <a href='#' className='text-gradient btn-link'>HTML</a>
-              <a href={HTMLpdf} target="_blank" className='btn w-7 h-7 mr-[42px] text-[16px] flex justify-center items-center'>
-                  <BsArrowUpRight />
-              </a>
-              <a href='#' className='text-gradient btn-link'>CSS</a>
-              <a href={CSSpdf} target="_blank" className='btn w-7 h-7 mr-[42px] text-[16px] flex justify-center items-center'>
-                  <BsArrowUpRight />
-              </a>
-              <a href='#' className='text-gradient btn-link'>JavaScript</a>
-              <a href={JSpdf} target="_blank" className='btn w-7 h-7 mr-[42px] text-[16px] flex justify-center items-center'>
-                  <BsArrowUpRight />
-              </a>
-              <a href='#' className='text-gradient btn-link'>.NET Full-Stack Foundation</a>
-              <a href={dotNETpdf} target="_blank" className='btn w-7 h-7 mr-[42px] text-[16px] flex justify-center items-center'>
-                  <BsArrowUpRight />
-              </a>
+            <h2 className='h2 text-accent font-semibold mb-12'>My Certifications</h2>
+            <div className='flex flex-col lg:flex-row text-[25px] gap-y-8 max-w-max mx-auto lg:mx-0 mb-28'>
+              <div className='flex flex-row gap-x-6'>
+                <a href='#' className='text-gradient btn-link'>HTML</a>
+                <a href={HTMLpdf} target="_blank" className='btn w-7 h-7 lg:mr-[42px] text-[16px] flex justify-center items-center'>
+                    <BsArrowUpRight />
+                </a>
+              </div>
+              <div className='flex flex-row gap-x-6'>
+                <a href='#' className='text-gradient btn-link'>CSS</a>
+                <a href={CSSpdf} target="_blank" className='btn w-7 h-7 lg:mr-[42px] text-[16px] flex justify-center items-center'>
+                    <BsArrowUpRight />
+                </a>
+              </div>
+              <div className='flex flex-row gap-x-6'>
+                <a href='#' className='text-gradient btn-link'>JavaScript</a>
+                <a href={JSpdf} target="_blank" className='btn w-7 h-7 lg:mr-[42px] text-[16px] flex justify-center items-center'>
+                    <BsArrowUpRight />
+                </a>
+              </div>
+              <div className='flex flex-row gap-x-6'>
+                <a href='#' className='text-gradient btn-link'>.NET Full-Stack Foundation</a>
+                <a href={dotNETpdf} target="_blank" className='btn w-7 h-7 mr-[42px] text-[16px] flex justify-center items-center'>
+                    <BsArrowUpRight />
+                </a>
+              </div>
             </div>
           </div>
         </div>
