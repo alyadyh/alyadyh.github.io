0649268e61dea668af4f9fe62842c21dcf4b4996
alyadyh <alya.mardhiyyah@binus.ac.id>
2023-10-21 09:53:41 +0700

    fix margin

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,7 +11,7 @@ const Nav = () => {
   return (
     <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
       <div className='container mx-auto'>
-        <div className='w-full bg-black/20 h-[90px] backdrop-blur-2x1 rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2x1 text-white/50'>
+        <div className='w-full bg-black/20 h-[75px] lg:h-[85px] backdrop-blur-2x1 rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2x1 text-white/50 mb-2 lg:mb-auto'>
           <Link to='home' activeClass='active' smooth={true} spy={true} offset={-200} className='cursor-pointer w-[52x] h-[52px] flex items-center justify-center'>
             <BiHomeAlt />
           </Link>
