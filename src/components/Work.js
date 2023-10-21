6b853a16047fe087b091019f3999f7a9a03a6262
alyadyh <alya.mardhiyyah@binus.ac.id>
2023-10-21 09:39:38 +0700

    fix margin

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,7 +8,7 @@ import { fadeIn } from '../variants';
 const Work = () => {
   return (
     <section className='section' id='work'>
-      <div className='container mx-auto'>
+      <div className='container mx-3 lg:mx-auto'>
         <div className='flex flex-col lg:flex-row gap-x-10'>
           <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
             <div>
