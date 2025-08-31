3a38ca90d8100caa69f498114f3b32585abbeb74
Alya Dhiya <91827571+alyadyh@users.noreply.github.com>
2025-08-31 23:08:41 +0700

    fix: updating about section styles

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -10,12 +10,12 @@ export const CardHeader = ({
   className?: string;
 }) => {
   return (
-    <div className={`flex flex-col ${className}`}>
+    <div className={`flex flex-col p-6 md:py-8 md:px-10 ${className}`}>
       <div className="inline-flex items-center gap-2">
         <StarIcon className="size-9 text-emerald-300" />
         <h3 className="text-3xl font-serif">{title}</h3>
       </div>
-      <p className="text-xl text-white/60 mt-2">{desc}</p>
+      <p className="text-sm lg:text-base text-white/60 mt-2">{desc}</p>
     </div>
   );
 };
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -146,7 +146,7 @@ const hobbies = [
 
 export const AboutSection = () => {
   return (
-    <div className="py-20">
+    <div className="py-20 lg:py-28">
       <div className="container">
         <SectionHeader
           title="About Me"
@@ -154,57 +154,61 @@ export const AboutSection = () => {
           desc="Learn more about who I am, what I do, and what inspires me."
         />
         <div className="mt-20 flex flex-col gap-8">
-          <Card className="h-[320px]">
-            <CardHeader
-              title="My Reads"
-              desc="Explore the book shaping my perspectives."
-            />
-            <div className="w-40 mx-auto mt-8">
-              <Image src={bookImage} alt="Book Cover" />
-            </div>
-          </Card>
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+              <CardHeader
+                title="My Reads"
+                desc="Explore the book shaping my perspectives."
+              />
+              <div className="w-40 mx-auto mt-2 md:mt-0">
+                <Image src={bookImage} alt="Book Cover" />
+              </div>
+            </Card>
 
-          <Card className="h-[320px] p-0">
-            <CardHeader
-              title="My Toolbox"
-              desc="Discover the tools and technologies behind my digital creations."
-              className="px-6 pt-6"
-            />
-            <ToolboxItems items={toolboxItems} className="mt-6" />
-            <ToolboxItems
-              items={toolboxItems}
-              className="mt-6"
-              itemWrapperClassName="-translate-x-1/2"
-            />
-          </Card>
+            <Card className="h-[320px] md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="My Toolbox"
+                desc="Discover the tools and technologies behind my digital creations."
+                className=""
+              />
+              <ToolboxItems items={toolboxItems} className="" />
+              <ToolboxItems
+                items={toolboxItems}
+                className="mt-6"
+                itemWrapperClassName="-translate-x-1/2"
+              />
+            </Card>
+          </div>
 
-          <Card className="h-[320px] p-0 flex flex-col">
-            <CardHeader
-              title="Beyond the Code"
-              desc="The little things that bring joy and inspiration to my life."
-              className="px-6 pt-6"
-            />
-            <div className="relative flex-1">
-              {hobbies.map((hobby) => (
-                <div key={hobby.title} className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-400 to-sky-400 rounded-full py-1.5 absolute"
-                  style={{
-                    left: hobby.left,
-                    top: hobby.top,
-                  }}
-                >
-                  <span className="font-medium text-gray-950">{hobby.title}</span>
-                  <span>{hobby.emoji}</span>
-                </div>
-              ))}
-            </div>
-          </Card>
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+            <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+              <CardHeader
+                title="Beyond the Code"
+                desc="The little things that bring joy and inspiration to my life."
+                className="px-6 pt-6"
+              />
+              <div className="relative flex-1">
+                {hobbies.map((hobby) => (
+                  <div key={hobby.title} className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-400 to-sky-400 rounded-full py-1.5 absolute"
+                    style={{
+                      left: hobby.left,
+                      top: hobby.top,
+                    }}
+                  >
+                    <span className="font-medium text-gray-950">{hobby.title}</span>
+                    <span>{hobby.emoji}</span>
+                  </div>
+                ))}
+              </div>
+            </Card>
 
-          <Card className="h-[320px] p-0 relative">
-            <Image src={mapImage} alt="Map" className="object-cover w-full h-full" />
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-400 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline-offset-2 after:rounded-full after:outline-gray-950/30 after:bg-gradient-to-r after:from-emerald-400 after:to-sky-400 after:-z-10">
-              <Image src={smileMemoji} alt="Smile Memoji" className="size-20" />
-            </div>
-          </Card>
+            <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
+              <Image src={mapImage} alt="Map" className="object-cover w-full h-full object-left-top" />
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-400 to-sky-400 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline-offset-2 after:rounded-full after:outline-gray-950/30 after:bg-gradient-to-r after:from-emerald-400 after:to-sky-400 after:-z-10">
+                <Image src={smileMemoji} alt="Smile Memoji" className="size-20" />
+              </div>
+            </Card>
+          </div>
         </div>
       </div>
     </div>
