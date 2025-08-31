7aad29d4207f47a667fa0ae3ab60814cf7fcd5ce
Alya Dhiya <91827571+alyadyh@users.noreply.github.com>
2025-09-01 00:21:39 +0700

    fix: adding mailto and set the icons to center

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -14,23 +14,25 @@ export const ContactSection = () => {
                 <h2 className="font-serif text-2xl md:text-3xl">Let&apos;s Connect</h2>
                 <p className="mt-2 md:text-base text-sm">I&apos;m always up for new opportunities&mdash;whether it&apos;s building cool projects, brainstorming fresh ideas, or teaming up to bring your vision to life.</p>
               </div>
-              <div className="flex gap-4">
+              <div className="flex gap-4 justify-center md:justify-start">
                 <a href='https://linkedin.com/in/alya-mardhiyyah/' target="_blank">
                   <FaLinkedin size={28} />
                 </a>
                 <a href='https://github.com/alyadyh/' target="_blank">
                   <FaGithub size={28} />
                 </a>
-                <a href='https:///instagram.com/alyadya_' target="_blank">
+                <a href='https://instagram.com/alyadya_' target="_blank">
                   <FaInstagram size={28} />
                 </a>
               </div>
             </div>
             <div>
-              <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
-                <span className="font-semibold">Contact Me</span>
-                <ArrowUpRight size={16} />
-              </button>
+              <a href="mailto:alya.mdyh@gmail.com?subject=Hello Alya!">
+                <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
+                  <span className="font-semibold">Contact Me</span>
+                  <ArrowUpRight size={16} />
+                </button>
+              </a>
             </div>
           </div>
         </div>
