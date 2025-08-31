0c2250663a5a53cbea3fff6d85669191b8bb542b
Alya Dhiya <91827571+alyadyh@users.noreply.github.com>
2025-09-01 01:34:12 +0700

    fix: add orbit animation

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -4,22 +4,30 @@ export const HeroOrbit = ({
   children,
   size,
   rotation,
-}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  shouldOrbit = false,
+  orbitDuration,
+  shouldSpin = false,
+  spinDuration,
+}: PropsWithChildren<{ size: number; rotation: number; shouldOrbit?: boolean; orbitDuration?: number; shouldSpin?: boolean; spinDuration?: number }>) => {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <div
-        className="flex items-start justify-start"
-        style={{
-          transform: `rotate(${rotation}deg)`,
-          width: `${size}px`,
-          height: `${size}px`,
-        }}
-      >
+      <div className={`${shouldOrbit === true ? 'animate-spin' : ''}`} style={{ animationDuration: `${orbitDuration}s` }}>
         <div
-          className="inline-flex"
-          style={{ transform: `rotate(${rotation * -1}deg)` }}
+          className="flex items-start justify-start"
+          style={{
+            transform: `rotate(${rotation}deg)`,
+            width: `${size}px`,
+            height: `${size}px`,
+          }}
         >
-          {children}
+          <div className={`${shouldSpin === true ? 'animate-spin' : ''}`} style={{ animationDuration: `${spinDuration}s` }}>
+            <div
+              className="inline-flex"
+              style={{ transform: `rotate(${rotation * -1}deg)` }}
+            >
+              {children}
+            </div>
+          </div>
         </div>
       </div>
     </div>
