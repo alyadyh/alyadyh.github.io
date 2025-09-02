7b3d6a61759f277496c4dbcccf8ff0b8aca0f8a5
Alya Dhiya <91827571+alyadyh@users.noreply.github.com>
2025-09-02 23:36:44 +0700

    fix: adding metadata logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,11 @@ const calistoga = Calistoga({
 export const metadata: Metadata = {
   title: "Alya Dhiya's Portfolio",
   description: "Personal Portfolio",
+  icons: {
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
