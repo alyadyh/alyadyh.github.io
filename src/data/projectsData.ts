1e366d7a1a39762d2a064eeb1662d2d37270c620
Alya Dhiya <91827571+alyadyh@users.noreply.github.com>
2025-09-09 23:54:09 +0700

    fix: adding project list, still error in icon manual color

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,6 +1,13 @@
 import eventRadarWebPage from "../assets/images/projects/event-radar-web.png";
 import ibarberpalPage from "../assets/images/projects/ibarberpal.png";
 import kulinariaPage from "../assets/images/projects/kulinaria.png";
+import pokedexPage from "../assets/images/projects/pokedex.png";
+import amostMobile from "../assets/images/projects/amost.png";
+import loneslyscapeAR from "../assets/images/projects/lonelyscape.png";
+
+import NativewindIcon from "../assets/icons/nativewind-logo.svg";
+// import ReactNativeIcon from "../assets/icons/react-native-logo.svg";
+import ReactNativeIcon from "../assets/icons/reactnative-logo.svg";
 
 import {
   SiBootstrap,
@@ -14,9 +21,64 @@ import {
   SiNodedotjs,
   SiBlender,
   SiCanva,
+  SiMui,
+  SiReact,
+  SiReactrouter,
+  SiTypescript,
+  SiAsana,
+  SiFigma,
+  SiExpo,
+  SiNextdotjs,
+  SiSupabase,
 } from "@icons-pack/react-simple-icons";
 
 export const portfolioProjects = [
+  {
+    company: "Full Stack Developer",
+    year: "2025",
+    title: "AMOST",
+    description:
+      "AMOST is a web application that allows users to create, read, update, and delete Pokémon data in an interactive digital Pokédex, also .",
+    icons: [
+      { title: "React Native", iconType: ReactNativeIcon },
+      { title: "NativeWind", iconType: NativewindIcon },
+      { title: "TypeScript", iconType: SiTypescript },
+      { title: "Expo Dev", iconType: SiExpo },
+      { title: "Supabase", iconType: SiSupabase },
+    ],
+    link: "https://drive.google.com/file/d/1lwtB8ZWmr5ds7MDLFqnF4EFWFuEgpzaX/view?usp=sharing",
+    image: amostMobile,
+  },
+  {
+    company: "Project Manager",
+    year: "2023",
+    title: "LonelyScape AR",
+    description: `LonelyScape AR is an an escape room game application using Augmented Reality (AR) technology. The method used in developing AR Escape Room: LonelyScape is Rapid Application Development (RAD).
+
+      Paper published in ICIMTech 2023
+      DOI: 10.1109/ICIMTech59029.2023.10277954`,
+    icons: [
+      { title: "Asana", iconType: SiAsana },
+      { title: "Figma", iconType: SiFigma },
+    ],
+    link: "https://drive.google.com/file/d/1kYRgwThY0vYDKs5AEVTBUQPlTx6W6KD4/view?usp=sharing",
+    image: loneslyscapeAR,
+  },
+  {
+    company: "Front-end Developer",
+    year: "2024",
+    title: "Pokedex",
+    description:
+      "Pokédex is a web application that allows users to create, read, update, and delete Pokémon data in an interactive digital Pokédex, also .",
+    icons: [
+      { title: "NextJS", iconType: SiNextdotjs },
+      { title: "Router DOM", iconType: SiReactrouter },
+      { title: "MUI", iconType: SiMui },
+      { title: "TypeScript", iconType: SiTypescript },
+    ],
+    link: "https://drive.google.com/file/d/17MLu59GXWoaj5B4mHKtSMSlnafTmggmc/view?usp=sharing",
+    image: pokedexPage,
+  },
   {
     company: "Front-end Developer",
     year: "2023",
