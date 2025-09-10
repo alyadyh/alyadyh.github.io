6e84cb63b8595b746260145fc5f06d2707ebec68
Alya Dhiya <91827571+alyadyh@users.noreply.github.com>
2025-09-10 13:52:01 +0700

    fix: build error final

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find(
